5bddcbe8c1aaf2eee4b50a3bd8b5a5272a0fe478
jayanthansenthilkumar <jayanthansenthilkumar18@gmail.com>
2025-04-13 10:37:50 +0530

    Unwanted lines are removed

--- cache.js
+++ cache.js
@@ -10,8 +10,7 @@ app.get('/', (req, res) => {
     const API_DOMAIN = 'https://fakestoreapi.com/';
     const response = axios.get(API_DOMAIN + 'products')
     return (await response).data;
-  } 
-
+  }
 app.get('/products', async(req, res) => {
     const products = await getProducts();
     res.send(products);
