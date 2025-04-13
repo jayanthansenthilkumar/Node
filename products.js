94abd9b6d2012442125bbdcc13141425f85e5050
jayanthansenthilkumar <jayanthansenthilkumar18@gmail.com>
2025-04-13 11:35:12 +0530

    Bugs cleared

--- products.js
+++ products.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/products', async (req, res) => {
+router.get('/', async (req, res) => {
     res.send({message: 'Your Products'});
 });
+
 router.get('/name', async (req, res) => {
     res.send({message: 'Name not found'});
 });
+
 module.exports = router;
